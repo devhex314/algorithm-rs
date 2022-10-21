7e76810ea2bd4a03ffd9b0957f935c4cf9cdb7b5
HyperNova114 <116303411+HyperNova114@users.noreply.github.com>
2022-10-21 22:25:51 +0530

    Create selection.rs

---
+++ src/sorting/selection.rs
@@ -0,0 +1,12 @@
+pub fn selection_sort<T: Ord>(arr: &mut [T]) {
+    let len = arr.len();
+    for left in 0..len {
+        let mut smallest = left;
+        for right in (left + 1)..len {
+            if arr[right] < arr[smallest] {
+                smallest = right;
+            }
+        }
+        arr.swap(smallest, left);
+    }
+}
