ae59221f6ee4fee701fcd2001a5324f32792e1fe
HyperNova114 <116303411+HyperNova114@users.noreply.github.com>
2022-10-23 23:44:35 +0530

    Update heap-sort.rs

--- src/sorting/heap-sort.rs
+++ src/sorting/heap-sort.rs
@@ -34,17 +34,3 @@ fn move_down<T: Ord>(arr: &mut [T], mut root: usize) {
         root = child;
     }
 }
-
-fn main() {
-    println!("Sort numbers ascending");
-    let mut numbers = [4, 65, 2, -31, 0, 99, 2, 83, 782, 1];
-    println!("Before: {:?}", numbers);
-    heap_sort(&mut numbers);
-    println!("After:  {:?}\n", numbers);
-
-    println!("Sort strings alphabetically");
-    let mut strings = ["beach", "hotel", "airplane", "car", "house", "art"];
-    println!("Before: {:?}", strings);
-    heap_sort(&mut strings);
-    println!("After:  {:?}\n", strings);
-}
